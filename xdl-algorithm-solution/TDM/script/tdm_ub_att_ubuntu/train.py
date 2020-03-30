47b57611416fead9da8341165833de2e1d4fb63c
INFRC <38290476+INFRC@users.noreply.github.com>
2020-03-31 00:20:59 +0800

    Update train.py
    调整网络定义，避免引入train阶段的batchSize，目前仅可跑通，准确性有待进一步验证。

--- xdl-algorithm-solution/TDM/script/tdm_ub_att_ubuntu/train.py
+++ xdl-algorithm-solution/TDM/script/tdm_ub_att_ubuntu/train.py
@@ -30,7 +30,7 @@ from tdm_layer_master import mx_dnn_layer, FullyConnected3D
 '''
   python train.py --run_mode=local --config=config.train.json
 '''
-
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 config = json.load(open('data/tdm.json', 'r'))
 def conf(key):
     return config[key].encode('utf8')
@@ -59,11 +59,13 @@ def init():
     from store import Store
     from dist_tree import DistTree
     #s = Store('size=10000000')
+    begin_time = time.time()
     s = Store(conf('tree_store_config'))
     s.load(conf('tree_pb'))
     tree = DistTree()
     tree.set_store(s.get_handle())
     tree.load()
+    print("JC Time "+"[init --> load tree] {}".format(time.time()-begin_time))
 
 def train(is_training=True):
     #np.set_printoptions(threshold='nan')
@@ -75,7 +77,7 @@ def train(is_training=True):
     file_type = xdl.parsers.txt
     if is_training:
         data_io = xdl.DataIO("tdm", file_type=file_type, fs_type=xdl.fs.hdfs,
-                             namenode="hdfs://your/namenode/hdfs/path:9000", enable_state=False)
+                             namenode="hdfs://localhost:8020", enable_state=False)
 
         feature_count = 69
         for i in xrange(1, feature_count + 1):
@@ -85,6 +87,8 @@ def train(is_training=True):
         data_io.batch_size(intconf('train_batch_size'))
         data_io.epochs(intconf('train_epochs'))
         data_io.threads(intconf('train_threads'))
+        tdmop_layer_counts = [int(i) for i in conf('tdmop_layer_counts').split(',')]
+        data_io.sgroup_queue_capacity(intconf('train_threads')*intconf('train_batch_size')/sum(tdmop_layer_counts))
         data_io.label_count(2)
         base_path = '%s/%s/' % (conf('upload_url'), conf('data_dir'))
         data = base_path + conf('train_sample') + '_' + r'[\d]+'
@@ -96,7 +100,7 @@ def train(is_training=True):
         iop = xdl.GetIOP("TDMOP")
     else:
         data_io = xdl.DataIO("tdm", file_type=file_type, fs_type=xdl.fs.hdfs,
-                             namenode="hdfs://your/namenode/hdfs/path:9000", enable_state=False)
+                             namenode="hdfs://localhost:8020", enable_state=False)
 
         feature_count = 69
         for i in xrange(1, feature_count + 1):
@@ -106,6 +110,8 @@ def train(is_training=True):
         data_io.batch_size(intconf('predict_batch_size'))
         data_io.epochs(intconf('predict_epochs'))
         data_io.threads(intconf('predict_threads'))
+        data_io.sgroup_queue_capacity(intconf('predict_io_pause_num') *
+                                      max(int(conf('pr_test_each_layer_retrieve_num')), int(conf('pr_test_final_layer_retrieve_num'))) * 2)
         data_io.label_count(2)
         base_path = '%s/%s/' % (conf('upload_url'), conf('data_dir'))
         data = base_path + conf('test_sample')
@@ -118,8 +124,8 @@ def train(is_training=True):
     key_value["key"] = "value"
     key_value["debug"] = conf('tdmop_debug')
     key_value["layer_counts"] = conf('tdmop_layer_counts')
-    key_value["pr_test_each_layer_retrieve_num"] = "400"
-    key_value["pr_test_final_layer_retrieve_num"] = "200"
+    key_value["pr_test_each_layer_retrieve_num"] = conf("pr_test_each_layer_retrieve_num")
+    key_value["pr_test_final_layer_retrieve_num"] = conf("pr_test_final_layer_retrieve_num")
     iop.init(key_value)
     data_io.add_op(iop)
     data_io.split_group(False)
@@ -148,17 +154,19 @@ def train(is_training=True):
     emb_name = "item_emb"
     for i in xrange(1, feature_count + 1):
         #emb_name = "item_%s_emb" % i
-        eb = xdl.embedding(emb_name, batch["item_%s" % i], xdl.Normal(stddev=0.001), emb_dim, 50000, emb_combiner, vtype="hash", feature_add_probability=feature_add_probability)
+        eb = xdl.embedding(emb_name, batch["item_%s" % i], xdl.Normal(stddev=0.001), emb_dim, 1000000, emb_combiner, vtype="hash", feature_add_probability=feature_add_probability)
         with xdl.device('GPU'):
             eb_take = xdl.take_op(eb, batch["indicators"][0])
         eb_take.set_shape(eb.shape)
         emb.append(eb_take)
     #emb_name = "unit_id_expand_emb"
-    unit_id_expand_emb = xdl.embedding(emb_name, batch["unit_id_expand"], xdl.Normal(stddev=0.001), emb_dim, 50000, emb_combiner, vtype="hash", feature_add_probability=feature_add_probability)
+    unit_id_expand_emb = xdl.embedding(emb_name, batch["unit_id_expand"], xdl.Normal(stddev=0.001), emb_dim, 1000000, emb_combiner, vtype="hash", feature_add_probability=feature_add_probability)
+
+    args = list()
+    auxs = list()
 
     @xdl.mxnet_wrapper(is_training=is_training, device_type='gpu')
-    def dnn_model_define(user_input, indicator, unit_id_emb, label, bs, eb_dim, fea_groups, active_op='prelu', use_batch_norm=True):
-        # 把用户输入按fea_groups划分窗口，窗口内做avg pooling
+    def dnn_model_define(user_input, indicator, unit_id_emb, label, eb_dim, fea_groups, active_op='prelu', use_batch_norm=True):
         fea_groups = [int(s) for s in fea_groups.split(',')]
         total_group_length = np.sum(np.array(fea_groups))
         print "fea_groups", fea_groups, "total_group_length", total_group_length, "eb_dim", eb_dim
@@ -167,16 +175,16 @@ def train(is_training=True):
     
         layer_data = []
         # start att
-        att_user_input = mx.sym.reshape(user_input, (bs, total_group_length, eb_dim))
-        att_node_input = mx.sym.reshape(unit_id_emb, (bs, 1, eb_dim))
+        att_user_input = mx.sym.reshape(user_input, (-1, total_group_length, eb_dim))
+        att_node_input = mx.sym.reshape(unit_id_emb, (-1, 1, eb_dim))
         att_node_input = mx.sym.broadcast_to(data=att_node_input, shape=(0, total_group_length, 0))
         att_din = mx.sym.concat(att_user_input, att_user_input * att_node_input, att_node_input, dim=2)
 
         att_active_op = 'prelu'
         att_layer_arr = []
-        att_layer1 = FullyConnected3D(3*eb_dim, 36, active_op=att_active_op, version=1, batch_size=bs)
+        att_layer1 = FullyConnected3D(3*eb_dim, 36, active_op=att_active_op, version="1", feature_count=feature_count)
         att_layer_arr.append(att_layer1)
-        att_layer2 = FullyConnected3D(36, 1, active_op=att_active_op, version=2, batch_size=bs)
+        att_layer2 = FullyConnected3D(36, 1, active_op=att_active_op, version="2", feature_count=feature_count)
         att_layer_arr.append(att_layer2)
 
         layer_data.append(att_din)
@@ -185,7 +193,7 @@ def train(is_training=True):
         att_dout = layer_data[-1]
         att_dout = mx.sym.broadcast_to(data=att_dout, shape=(0, 0, eb_dim))
 
-        user_input = mx.sym.reshape(user_input, shape=(bs, -1, eb_dim))
+        #user_input = mx.sym.reshape(user_input, shape=(bs, -1, eb_dim))
         user_input = user_input * att_dout
         # end att
 
@@ -201,7 +209,6 @@ def train(is_training=True):
     
         indicator = mx.symbol.BlockGrad(indicator)
         label = mx.symbol.BlockGrad(label)
-        # 按indicator来扩展user fea，然后过网络
         #grouped_user_input_after_take = mx.symbol.take(grouped_user_input, indicator)
         grouped_user_input_after_take = grouped_user_input
         din = mx.symbol.concat(*[grouped_user_input_after_take, unit_id_emb], dim=1)
@@ -221,23 +228,27 @@ def train(is_training=True):
         for layer in layer_arr:
             layer_data.append(layer.call(layer_data[-1]))
         dout = layer_data[-1]
-    
-        # 正常label两列加和必为1，补全的label为0，故减一之后即可得到-1，作为ignore label
         ph_label_sum = mx.sym.sum(label, axis=1)
         ph_label_ignore = ph_label_sum - 1
         ph_label_ignore = mx.sym.reshape(ph_label_ignore, shape=(-1, 1))
         ph_label_click = mx.sym.slice_axis(label, axis=1, begin=1, end=2)
         ph_label_click = ph_label_click + ph_label_ignore
-        ph_label_click = mx.sym.reshape(ph_label_click, shape=(bs, ))
+        ph_label_click = mx.sym.reshape(ph_label_click, shape=(-1, ))
+        #ph_label_click = mx.sym.expand_dims(ph_label_click, axis=1)
     
+        #prop = mx.symbol.SoftmaxOutput(data=dout, label=label, grad_scale=1.0, use_ignore=True, normalization='valid')
         prop = mx.symbol.SoftmaxOutput(data=dout, label=ph_label_click, grad_scale=1.0, use_ignore=True, normalization='valid')
+        xdl.graph_tag().set_mx_output(prop)
+        args.extend(prop.list_arguments())
+        auxs.extend(prop.list_auxiliary_states())
+
         origin_loss = mx.sym.log(prop) * label
-        ph_label_sum = mx.sym.reshape(ph_label_sum, shape=(bs, 1))
+        ph_label_sum = mx.sym.reshape(ph_label_sum, shape=(-1, 1))
+        #ph_label_sum = mx.sym.expand_dims(ph_label_sum, axis=1)
         origin_loss = mx.sym.broadcast_mul(origin_loss, ph_label_sum)
         loss = - mx.symbol.sum(origin_loss) / mx.sym.sum(ph_label_sum)
         return prop, loss
-
-    re = dnn_model_define(emb, batch["indicators"][0], unit_id_expand_emb, batch["label"], data_io._batch_size, emb_dim, '20,20,10,10,2,2,2,1,1,1')
+    re = dnn_model_define(emb, batch["indicators"][0], unit_id_expand_emb, batch["label"], emb_dim, '20,20,10,10,2,2,2,1,1,1')
     prop = re[0]
     loss = re[1]
 
@@ -269,6 +280,7 @@ def train(is_training=True):
     elapsed_time = 0.
     statis_begin_loop = 200
     loop_num = 0
+    begin_time2 = time.time()
     while not sess.should_stop():
         print ">>>>>>>>>>>> %d >>>>>>>>>>>" % loop_num
         begin_time = time.time()
@@ -290,10 +302,10 @@ def train(is_training=True):
             loop_num += 1
         if loop_num > statis_begin_loop:
             elapsed_time += time.time() - begin_time
-            #print 'batch_size = %d, qps = %f batch/s' % (data_io._batch_size, (loop_num - statis_begin_loop) / elapsed_time)
+            print 'batch_size = %d, qps = %f batch/s' % (data_io._batch_size, (loop_num - statis_begin_loop) / elapsed_time)
 
+    print("JC Time "+"[dnn train] {}".format(time.time()-begin_time2))
     if is_training:
-        xdl.execute(xdl.ps_synchronize_leave_op(np.array(xdl.get_task_index(), dtype=np.int32)))
         if xdl.get_task_index() == 0:
             print 'start put item_emb'
             def _string_to_int8(src):
@@ -309,10 +321,19 @@ def train(is_training=True):
             shell_cmd("sed -i 's/..//' data/item_emb")
             shell_cmd("hadoop fs -put -f data/item_emb %s" % output_dir)
             print 'finish put item_emb'
-        #print 'before worker barrier'
-        #xdl.execute(xdl.worker_barrier_op(np.array(xdl.get_task_index(), dtype=np.int32), np.array(xdl.get_task_num(), dtype=np.int32)))
-        #print 'after worker barrier'
+
+            is_export = True
+            if is_export:
+                print 'start export'
+                from xdl.python.utils.config import get_ckpt_dir
+                from xdl.python.training.export import export_sparse, export_dense_mx
+                export_dir=output_dir+'/export'
+                export_sparse(get_ckpt_dir(), export_dir, ['item_emb'], 'hash')
+                export_dense_mx(get_ckpt_dir(), args, auxs, export_dir)
+                xdl.graph_tag().set_input(data_io)
+                saver = xdl.Saver(get_ckpt_dir())
+                saver.export_graph(export_dir, True)
+                print 'finish export, export_dir=%s' % export_dir
 
 train(is_training=True)
 #train(is_training=False)
-
